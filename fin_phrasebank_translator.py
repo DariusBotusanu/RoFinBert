814c7fe2eb9d47724360ae96d70243ec1121ed9e
Darius Botusanu <botusanu.darius31@outlook.com>
2024-04-17 10:35:58 +0300

    Ensured data persistence when translating

--- fin_phrasebank_translator.py
+++ fin_phrasebank_translator.py
@@ -2,13 +2,21 @@ from datasets import load_dataset
 from transformers import pipeline
 from tqdm import tqdm
 
-def translate_save(sentence, translation_pipe=None, file_path='./datasets/all_agree_ro.txt'):
-    translated_sentence = translation_pipe(sentence)[0]['translation_text']
+def translate_sentences(sentences=None, translation_pipe=None, file_path='./datasets/all_agree_ro.txt'):
+    '''
+    Translates sentences from a dataset. Progress is tracked in a file and can be resumed if the program stops.
+    '''
+    with open(file_path, 'r', encoding="utf-8") as f:
+        current_line = len(f.readlines())
+        
     with open(file_path, 'a+', encoding="utf-8") as f:
-        f.write(translated_sentence)
-        f.write('\n')
-
-
+        for sentence in tqdm(sentences[current_line:]):
+            translated_sentence = translation_pipe(sentence)[0]['translation_text']
+            f.write(translated_sentence)
+            f.write('\n')
+            f.flush()
+        
+        
 if __name__=="__main__":
     #Load the financial_phrasebank dataset where all labelers agree
     dataset = load_dataset("financial_phrasebank", 'sentences_allagree', trust_remote_code=True)
@@ -16,13 +24,4 @@ if __name__=="__main__":
     #Make a pipeline to translate from English to Romanian
     pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-tc-big-en-ro")
 
-    for sentence in tqdm(dataset['train']['sentence']):
-        translate_save(sentence, translation_pipe=pipe)
-
-    # #Translate all sequences to Romanian
-    # print("Translating Sequences...")
-    # dataset['train']['sentence'] = pipe(dataset['train']['sentence'])
-    #                          
-    # #Save the dataset to disk
-    # print("Saving to disk...")
-    # dataset.save_to_disk("datasets/ro_financial_phrasebank_all_agree.hf")
+    translate_sentences(sentences=dataset['train']['sentence'], translation_pipe=pipe, file_path='./datasets/all_agree_ro.txt')
