bd873907e3a87635f37197c8e4e4cb0aec8e15e0
Darius Botusanu <botusanu.darius31@outlook.com>
2024-05-29 20:24:43 +0300

    Parallelised translations and removed bad datasets

--- fin_phrasebank_translator.py
+++ fin_phrasebank_translator.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 from transformers import pipeline
+import concurrent.futures
 from tqdm import tqdm
 
 def translate_sentences(sentences=None, translation_pipe=None, file_path='./datasets/all_agree_ro.txt'):
@@ -15,17 +16,20 @@ def translate_sentences(sentences=None, translation_pipe=None, file_path='./data
             f.write(translated_sentence)
             f.write('\n')
             f.flush()
-        
-        
-if __name__=="__main__":
-    #Load the financial_phrasebank dataset where all labelers agree
-    dataset = load_dataset("financial_phrasebank", 'sentences_allagree', trust_remote_code=True)
 
+def load_and_translate(dataset_name, file_path):
+    dataset = load_dataset("financial_phrasebank", dataset_name, trust_remote_code=True)
+    translate_sentences(sentences=dataset['train']['sentence'], translation_pipe=pipe, file_path=file_path)
+
+
+if __name__=="__main__":
     #Make a pipeline to translate from English to Romanian
     pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-tc-big-en-ro")
-
-    translate_sentences(sentences=dataset['test']['sentence'], translation_pipe=pipe, file_path='./datasets/test_all_agree_ro.txt')
     
-##Studiaza pe ultimul strat embeddingurile
-##Set de embeddings pentru cuvinte specifice domeniului
-##TFIDF, comparare intre clasificari
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        futures.append(executor.submit(load_and_translate, 'sentences_allagree', './datasets/all_agree_ro.txt'))
+        futures.append(executor.submit(load_and_translate, 'sentences_75agree', './datasets/75_agree_ro.txt'))
+
+    for future in concurrent.futures.as_completed(futures):
+        print(f"Task Completed: {future.result()}")
